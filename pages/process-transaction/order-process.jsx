fcbdcd343472dab99db540cc0d998b39c0f9c338
Wilgen Sanchez <Wilgensanchez98@gmail.com>
2021-06-01 10:42:17 -0600

    Metodo Asincrono para llamar al API en el efecto

--- pages/process-transaction/order-process.jsx
+++ pages/process-transaction/order-process.jsx
@@ -3,35 +3,45 @@ import { useRouter } from 'next/router';
 import { Flex, Heading } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 
+
+
 const transaction = () => {
     const router = useRouter();
     const [message, setmessage] = useState('');
     const [code, setcode] = useState('');
     useEffect(() => {
-        if (router.query.id === undefined) {
-            return;
-        }
-
-        fetch(
-            process.env.NEXT_PUBLIC_REST_API + '/paymentPH1/' + router.query.id,
-            {
-                method: 'POST',
-            }
-        )
-            .then((res) => res.json())
-            .then((res) => {
-                console.log(res.status);
-                console.log(res.status.code);
-
-                setmessage(res.status.message + '\n');
-                setcode(result.status.code);
-
-                console.log(code);
-
-            })
-            .catch(function (error) {
+     
+            async function verifyTransaction() {
+                if (router.query.id === undefined) {
+                    return;
+                }
+                
+              try {
+                await fetch(
+                    process.env.NEXT_PUBLIC_REST_API + '/paymentPH1/' + router.query.id,
+                    {
+                        method: 'POST',
+                    }
+                )
+                    .then((res) => res.json())
+                    .then((res) => {
+                        console.log(res.status);
+                        console.log(res.status.code);
+        
+                        setmessage(res.status.message + '\n');
+                        setcode(result.status.code);
+        
+                        console.log(code);
+                    })
+                    .catch(function (error) {
+                        console.log('The error is handled, continue normally. ', error);
+                    });
+              } catch (error) {
                 console.log('The error is handled, continue normally. ', error);
-            });
+              }
+            }
+
+            verifyTransaction();
     }, [router.query.id]);
 
     return (
@@ -43,7 +53,14 @@ const transaction = () => {
                 p={12}
                 rounded={10}
             >
-                <Heading color={code == process.env.NEXT_PUBLIC_API_SUCCESS_RESPONSE ? 'green' : 'red'} mb={10}>
+                <Heading
+                    color={
+                        code == process.env.NEXT_PUBLIC_API_SUCCESS_RESPONSE
+                            ? 'green'
+                            : 'red'
+                    }
+                    mb={10}
+                >
                     {code == process.env.NEXT_PUBLIC_API_SUCCESS_RESPONSE
                         ? 'Transaccion completada en modo test'
                         : 'Error: ' + message}
