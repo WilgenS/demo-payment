3eee3664e3661308685d2bb628480cb4aaf5bf7f
Wilgen Sanchez <Wilgensanchez98@gmail.com>
2021-06-01 10:46:12 -0600

    validación del load heading

--- pages/process-transaction/order-process.jsx
+++ pages/process-transaction/order-process.jsx
@@ -8,40 +8,31 @@ import { useState, useEffect } from 'react';
 const transaction = () => {
     const router = useRouter();
     const [message, setmessage] = useState('');
-    const [code, setcode] = useState('');
+    const [code, setcode] = useState(null);
     useEffect(() => {
-     
-            async function verifyTransaction() {
-                if (router.query.id === undefined) {
-                    return;
-                }
-                
-              try {
-                await fetch(
-                    process.env.NEXT_PUBLIC_REST_API + '/paymentPH1/' + router.query.id,
-                    {
-                        method: 'POST',
-                    }
-                )
-                    .then((res) => res.json())
-                    .then((res) => {
-                        console.log(res.status);
-                        console.log(res.status.code);
-        
-                        setmessage(res.status.message + '\n');
-                        setcode(result.status.code);
-        
-                        console.log(code);
-                    })
-                    .catch(function (error) {
-                        console.log('The error is handled, continue normally. ', error);
-                    });
-              } catch (error) {
-                console.log('The error is handled, continue normally. ', error);
-              }
-            }
+        if (router.query.id === undefined) {
+            return;
+        }
 
-            verifyTransaction();
+        fetch(
+            process.env.NEXT_PUBLIC_REST_API + '/paymentPH1/' + router.query.id,
+            {
+                method: 'POST',
+            }
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res.status);
+                console.log(res.status.code);
+
+                setmessage(res.status.message + '\n');
+                setcode(result.status.code);
+
+                console.log(code);
+            })
+            .catch(function (error) {
+                console.log('The error is handled, continue normally. ', error);
+            });
     }, [router.query.id]);
 
     return (
@@ -53,7 +44,7 @@ const transaction = () => {
                 p={12}
                 rounded={10}
             >
-                <Heading
+               { code && <Heading
                     color={
                         code == process.env.NEXT_PUBLIC_API_SUCCESS_RESPONSE
                             ? 'green'
@@ -64,7 +55,7 @@ const transaction = () => {
                     {code == process.env.NEXT_PUBLIC_API_SUCCESS_RESPONSE
                         ? 'Transaccion completada en modo test'
                         : 'Error: ' + message}
-                </Heading>
+                </Heading>}
             </Flex>
         </Flex>
     );
